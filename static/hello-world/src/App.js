bdb7f4ea50d4f0a5d52820bae0a8df3f68046085
Shivansh Pandey <you@example.com>
2025-07-29 17:53:25 +0530

    last commit

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -3,18 +3,23 @@ import { invoke, view } from '@forge/bridge';
 import Home from './components/Home';
 
 function App() {
-  const [projectKey, setProjectKey] = useState(null);
+  // const [projectKey, setProjectKey] = useState(null);
   const [allIssues, setAllIssues] = useState([]);
 
   useEffect(() => {
     const fetchContext = async () => {
       const context = await view.getContext();
       const key = context?.extension?.project?.key;
-      setProjectKey(key);
+      console.log('Forge Context:', context);
+      console.log('Project Key:', key);
+
+      // setProjectKey(key);
+      // await invoke('getProjectKey', { projectKey: key });
 
       try {
         const issues = await invoke('getIssues', { projectKey: key });
         setAllIssues(issues);
+        console.log('Fetched Issues:', issues);
       } catch (error) {
         console.error('Error fetching issues:', error);
         setAllIssues([]);
