6576f31ae2d69c47fb2f3d266a07945540bf45f9
Shivansh Pandey <you@example.com>
2025-08-04 13:08:20 +0530

    first commit

--- static/hello-world/src/components/workflow-aging/WorkFlowAging.js
+++ static/hello-world/src/components/workflow-aging/WorkFlowAging.js
@@ -11,7 +11,7 @@ import ArrowUpIcon from '@atlaskit/icon/glyph/arrow-up';
 import ArrowDownIcon from '@atlaskit/icon/glyph/arrow-down';
 import StatusTimelineChart from "./StatusTimelineChart";
 
-const WorkFlowAging = ({ data, filters, onBack }) => {
+const WorkFlowAging = ({ data, filters }) => {
   const [globalFilter, setGlobalFilter] = useState("");
   const [sorting, setSorting] = useState([]);
   const [columnFilters, setColumnFilters] = useState([]);
